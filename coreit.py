150cec8a5d9b726749af54e85e8daa253b9b07ad
matt <matthewharrison@gmail.com>
2010-07-01 12:47:00 -0600

    remove commented code

--- coreit.py
+++ coreit.py
@@ -53,7 +53,6 @@ def coreit(functions, args, num_procs):
 def slow_function():
     import math
     total = 0
-    #for i in xrange(100000000):
     for i in xrange(10000000):
         total = total + math.sqrt(i)
     print total
@@ -70,45 +69,3 @@ def test():
 
 if __name__ == '__main__':
     test()
-
-# def dump_fact_multiprocess(dburl, start_id, end_id, num_procs=3):
-#     from multiprocessing import Process, Queue
-#     work_q = Queue()
-#     for i in range(start_id, end_id):
-#         work_q.put(i)
-#     processes = [Process(target=dump_from_q, args=(dburl, work_q)) for i in ran\
-# ge(num_procs)]
-#     for p in processes:
-#         p.start()
-#     for p in processes:
-#         p.join()
-
-# def dump_from_q(dburl, q):
-#     from Queue import Empty
-#     while True:
-#         try:
-#             id = q.get(block=False)
-#             print "DUMPING", id
-#             dump(dburl, id)
-#         except Empty:
-#             break
-
-
-# def dump_fact(start_id, end_id):
-#     for i in range(start_id, end_id):
-#         print 'dumping', i
-#         dump('oracle://app_whs:password@192.168.100.204/item', i)
-
-
-
-# def to_csv(data, filename, write_headers=True):
-#     """
-#     write data (sqlalchemy results, to a csv file that postgres can bulk load)
-#     """
-#     fout = open(filename, 'w')
-#     os.chmod(filename, 0777) # postgres user needs read permission                  #import pdb; pdb.set_trace()                                                    first_row = data.next()
-#     fieldnames = [col for col in first_row.keys()]
-#     #csv_out = csv.DictWriter(fout, delimiter='|', fieldnames=fieldnames, quote\
-# -uu-:---F1  dumporacle.py   Top L1     (Python)---------------------------------
-# Mark set
-# [ caserver01 ][                 (0*$ bash)                  ][2010-07-01  8:04 ]
